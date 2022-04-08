7c8a0a2710c8976a765e0b698a62b7cb53969c14
RaulCaro28 <raulcaro329@gmail.com>
2022-04-08 15:48:18 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './home/header/header.component';
+import { ContentComponent } from './home/content/content.component';
+import { FooterComponent } from './home/footer/footer.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
